225fda09a14a948572a2c5af576031fc03b9a95b
Jourdan Clark <jourdan_clark@hotmail.com>
2018-02-12 14:45:38 -0700

    Resolving conflicts

--- src/server/controllers/users.js
+++ src/server/controllers/users.js
@@ -53,5 +53,6 @@ const updateUser = async (req, res, next) => {
 };
 
 module.exports = {
-  getUser
+  getUser,
+  convertToOtherUserType
 };
--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const routes = require('./routes');
 
 const PORT = process.env.PORT || '3001';
 
 const app = express();
+
+app.use(
+cors({
+  allowedHeaders:['Content-Type', 'Authorization'],
+  credentials: true
+}));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());

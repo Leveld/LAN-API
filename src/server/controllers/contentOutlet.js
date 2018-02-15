b23440b94dc1809b98897d2a29d5fda50d76b321
Jourdan Clark <jourdan_clark@hotmail.com>
2018-02-15 15:31:55 -0700

    Working on ContentOutlet routes

--- src/server/controllers/contentOutlet.js
+++ src/server/controllers/contentOutlet.js
@@ -1,7 +1,19 @@
 const axios = require('axios');
+const base64 = require('base64-url');
+const { google } = require('googleapis');
+const plus = google.plus('v1');
+const OAuth2Client = google.auth.OAuth2;
 const { throwError, authServerIP, dbServerIP } = require('capstone-utils');
 
-const { getToken : getUserToken } = require('./user');
+const { getToken : getUserToken, getUserFromToken  } = require('./user');
+
+const oauth2Client = new OAuth2Client(
+  '660421589652-k537cl8vg3v8imub4culbjon6f20fph6.apps.googleusercontent.com',
+  'yYuc3V2fIT4DOfnZXIyhBvsh',
+  `http://localhost:3002/oauth`
+);
+
+google.options({ auth: oauth2Client });
 
 const getOutletToken = async (contentOutlet) => {
   let token = await axios.get(`${authServerIP}cotoken`, {
@@ -17,6 +29,50 @@ const getOutletToken = async (contentOutlet) => {
   return token.token;
 };
 
+// GET /coURL
+const generateURL = async (req, res, next) => {
+  const { type = '', redirect = '' } = req.query;
+  const user = await getUserFromToken(getUserToken(req));
+  // TODO throw error if user wasn't found
+  const userID = base64.encode(user._id);
+  const userType = base64.encode(user.type);
+
+  await res.status(307).redirect(`${authServerIP}coURL?type=${type}&redirect=${redirect}&userID=${userID}&userType=${userType}`);
+}
+
+// GET /coInfo
+const getContentOutletInfo = async (req, res, next) => {
+  // call getOutletToken to get access token
+  const { id } = req.query;
+  const tokens = await getOutletToken(id);
+  // call setCredentials on the OAuth2Client object with the token
+  oauth2Client.setCredentials(tokens);
+  const youtube = google.youtube({
+    version: 'v3'
+  });
+  youtube.channels.list({
+    "part": "snippet",
+    "mine": "true"
+  }, (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    const channelId = data.data.items[0].id;
+    const channelLink = `https://www.youtube.com/channel/${channelId}`;
+    const profilePicture = data.data.items[0].snippet.thumbnails.default.url;
+    const channelName = data.data.items[0].snippet.localized.title;
+    const channelInfo = {
+      channelName,
+      profilePicture,
+      channelLink
+    }
+
+    return await res.send({ channelInfo });
+  });
+}
+
+// GET /outlet
 const getOutlet = async (req, res, next) => {
   const token = getUserToken(req);
   if (!token)
@@ -44,20 +100,9 @@ const getOutlet = async (req, res, next) => {
   Object.assign(outlet, { accessToken: contentToken });
 
   await res.send(outlet);
-
-//   {
-//    username: 'bob',
-//     channelID: 'laksdjflkajsldkfjalskdjfsa',
-//     profilePicture: 'google.com/aslkdjflaksjdflaksjdf',
-//     channelURL: 'youtube.com/bob',
-//     owner: {
-//      ownerID: 'alksdjfiwjleikfjlaisjdfljwe',
-//       ownerType: 'Business'
-//     },
-//    accessToken: <retrieve the token>
-//   }
 };
 
+// POST /outlet
 const createOutlet = async (req, res, next) => {
   const { fields } = req.body;
   if (typeof fields !== 'object')
@@ -74,6 +119,7 @@ const createOutlet = async (req, res, next) => {
   await res.send(outlet);
 };
 
+// PATCH /outlet
 const updateOutlet = async (req, res, next) => {
   const { id, fields } = req.body;
   if (typeof id !== 'string')
@@ -92,6 +138,7 @@ const updateOutlet = async (req, res, next) => {
 
   await res.send(outlet);
 };
+
 
 module.exports {
   getOutlet,

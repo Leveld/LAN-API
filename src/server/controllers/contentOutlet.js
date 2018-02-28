162f11c72018ed3fe980fb07f78777b408551534
Jourdan Clark <jourdan_clark@hotmail.com>
2018-02-27 19:17:09 -0700

    Added support for campaigns and contracts

--- src/server/controllers/contentOutlet.js
+++ src/server/controllers/contentOutlet.js
@@ -1,40 +1,10 @@
 const axios = require('axios');
-const base64 = require('base64-url');
-const { google } = require('googleapis');
-const plus = google.plus('v1');
-const OAuth2Client = google.auth.OAuth2;
-const { throwError, authServerIP, dbServerIP, googleRedirect } = require('capstone-utils');
-const {googleClientID, googleClientSecret} = require('../secret.json');
-
-const { getToken : getUserToken, getUserFromToken  } = require('./users');
-
-const oauth2Client = new OAuth2Client(
-  googleClientID,
-  googleClientSecret,
-  googleRedirect
-);
-
-google.options({ auth: oauth2Client });
-
-const getOutletToken = async (contentOutlet) => {
-  let token = await axios.get(`${authServerIP}cotoken`, {
-    params: {
-      contentOutlet
-    }
-  });
-
-
-  if (!token || !token.data || !token.data.token)
-    throwError('DBContentOutlet', 'No token found for specified contentOutlet');
-
-  token = token.data;
-  return { accessToken: token.token, refreshToken: token.refreshToken, expires: new Date(token.expires) };
-};
+const { throwError, authServerIP, dbServerIP } = require('capstone-utils');
 
 // GET /coURL
 const generateURL = async (req, res, next) => {
   const { type = '', redirect = '' } = req.query;
-  const user = await getUserFromToken(getUserToken(req));
+  const user = req.authedUser;
 
   let url = await axios.get(`${authServerIP}coURL`, {
     params: {
@@ -53,48 +23,26 @@ const generateURL = async (req, res, next) => {
 
 // GET /coInfo
 const getContentOutletInfo = async (req, res, next) => {
-  // call getOutletToken to get access token
   const { id } = req.query;
-  const { accessToken, refreshToken, expires } = await getOutletToken(id);
 
-  // call setCredentials on the OAuth2Client object with the token
-  oauth2Client.setCredentials({ access_token: accessToken, refresh_token: refreshToken, expiry_date: expires.valueOf() });
-  const youtube = google.youtube({
-    version: 'v3'
+  let outletInfo = await axios.get(`${dbServerIP}coInfo`, {
+    params: {
+      id
+    }
   });
 
-  const callback = async (error, response) => {
-    try {
-      if (error)
-        throw error;
+  if (!outletInfo)
+    outletInfo = null;
+  else
+    outletInfo = outletInfo.data;
 
-      const channelID = response.data.items[0].id;
-      const channelLink = `https://www.youtube.com/channel/${channelID}`;
-      const profilePicture = response.data.items[0].snippet.thumbnails.default.url;
-      const channelName = response.data.items[0].snippet.localized.title;
-      const channelInfo = { channelID, channelLink, profilePicture, channelName };
-
-      await res.send(channelInfo);
-    } catch (error) {
-      next(error);
-    }
-  };
-
-  youtube.channels.list({
-    "part": "snippet",
-    "mine": "true"
-  }, callback);
-}
+  await res.send(outletInfo);
+};
 
 // GET /outlet
 const getOutlet = async (req, res, next) => {
-  const token = getUserToken(req);
-  if (!token)
-    throwError('APIAuthenticationError', 'Missing Authentication');
-
+  const token = req.authToken;
   const { id } = req.query;
-
-  const { accessToken } = await getOutletToken(id);
 
   let outlet = await axios.get(`${dbServerIP}cotoken`, {
     params: {
@@ -107,16 +55,16 @@ const getOutlet = async (req, res, next) => {
 
   outlet = outlet.data;
 
-  Object.assign(outlet, { accessToken });
-
   await res.send(outlet);
 };
 
 // POST /outlet
 const createOutlet = async (req, res, next) => {
   const { fields } = req.body;
+
   if (typeof fields !== 'object')
-    throwError('DBContentOutlet', `Missing parameter 'fields'`);
+    throwError('APIContentOutletError', `Missing parameter 'fields'`);
+
   const outlet = axios.post(`${dbServerIP}cotoken`, {
     fields
   });
@@ -132,10 +80,12 @@ const createOutlet = async (req, res, next) => {
 // PATCH /outlet
 const updateOutlet = async (req, res, next) => {
   const { id, fields } = req.body;
+
   if (typeof id !== 'string')
-    throwError('DBContentOutlet', `Missing parameter 'id'`);
+    throwError('APIContentOutletError', `Missing parameter 'id'`);
   if (typeof fields !== 'object')
-    throwError('DBContentOutlet', `Missing parameter 'fields'`);
+    throwError('APIContentOutletError', `Missing parameter 'fields'`);
+
   const outlet = axios.patch(`${dbServerIP}cotoken`, {
     id,
     fields
@@ -151,8 +101,6 @@ const updateOutlet = async (req, res, next) => {
 
 // GET /outlets
 const getOutlets = async (req, res, next) => {
-  const token = getUserToken(req);
-
   let outlets = await axios.get(`${dbServerIP}outlets`);
 
   if (outlets)
--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { throwError, asyncMiddleware } = require('capstone-utils');
 
 const routes = require('./routes');
 
@@ -16,6 +17,22 @@ app.use(cors({
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+const getTokenAndUser = async (req, res, next) => {
+  req.authToken = req.header('Authorization') ? req.header('Authorization').split('Bearer ').splice(0).join(' ').trim() : null;
+  if (req.authToken === null)
+    throwError('APIAuthenticationError', 'Missing Authorization Token', 403);
+  try {
+     const user = await axios.get(`${apiServerIP}user`, { headers: { Authorization: `Bearer ${token}`}, withCredentials: true });
+     if (user)
+      req.authedUser = user.data;
+  } catch (error) {
+    throwError('APIAuthenticationError', 'Invalid Authorization Token', 403);
+  }
+  next();
+};
+
+app.use(asyncMiddleware(getTokenAndUser));
 
 routes(app);
 
